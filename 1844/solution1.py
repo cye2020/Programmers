f0f4201daaef26ae7c47d47d1d4cc84cad8d5bf2
cye2020 <ariaplay101@gmail.com>
2023-01-09 04:38:47 +0900

    folder name

---
+++ 1844/solution1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from collections import deque
+
+
+def bfs(maps):
+    n, m = np.shape(maps)
+    print(n, m)
+    answer = -1
+    
+
+def solution(maps):
+    answer = 0
+    bfs(maps)
+    return answer
